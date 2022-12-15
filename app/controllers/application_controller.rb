1c2a9a662049c264ca6bead1dc30fab63d0c8532
kamkara <lnclass.kamkara@gmail.com>
2022-12-15 17:49:08 +0000

    Add Membership

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+ protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    
+    protected
+    # If you have extra params to permit, append them to the sanitizer.
+    def configure_permitted_parameters
+        sign_up_params = [ :first_name, :last_name, :full_name, :status,
+                        :country, :email, :avatar, :password, :slug,
+                            :gender, :full_contact]
+            
+        #Devise strong Params
+        devise_parameter_sanitizer.permit :sign_up, keys: sign_up_params
+        devise_parameter_sanitizer.permit :account_update, keys: sign_up_params
+        devise_parameter_sanitizer.permit :sign_in, keys: [:email, :password]
+    end
+
+    def after_sign_in_path_for(resource)
+        dashboard_path
+    end
+
+    def after_sign_up_path_for(resource)
+        dashboard_path
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,42 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
+
+
+  has_one_attached :avatar
+  
+  STATUS= ["Editeur", "Level 2", "Level 3"]
+
+  def full_name
+    "#{self.first_name} #{self.last_name}" 
+  end
+
+  validates :first_name, :last_name, :full_name, :email, :password,
+              :status, :country, :full_contact, presence: true
+    
+   validates :full_name,presence: true,
+              format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ },
+              length: { minimum:5, maximum: 30,
+              message: "%{value} verifier votre nom complet"}
+
+                
+
+   ################## SLUG ###############
+  extend FriendlyId
+  friendly_id :full_name, use: :slugged
+  
+  def should_generate_new_friendly_id?
+    full_name_changed?
+  end 
+ 
+  
+  ################## BEFORE SAVE  #########
+  before_save do
+    #self.country            = country.strip.squeeze(" ")
+    self.full_contact            = full_contact .strip.squeeze(" ").capitalize
+    self.first_name         = first_name.strip.squeeze(" ").downcase.capitalize
+    self.last_name          = last_name.strip.squeeze(" ").downcase.capitalize
+  end
+
 end
